983548057f68fb572d43b08223d1834ff891b9aa
ggkitsas <ggkitsas@yahoo.com>
2014-07-15 15:23:56 +0300

    csGetPublicKey and csDecipher

--- src/cryptostick.c
+++ src/cryptostick.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "common.h"
@@ -29,7 +30,10 @@ int csGetSerialNo(card_t *card, unsigned char serialno[6])
     int r;
 
     connect_card(card->reader, &card);
+    pcsc_connect(card->reader);
+
     memcpy(serialno, &(card->serialnr.value[2]), 6);
+
     pcsc_disconnect(card->reader);
     return 0;
 }
@@ -39,8 +43,41 @@ int csGetPublicKey(card_t *card, unsigned char* public_key)
     int r;
 
     connect_card(card->reader, &card);
+    pcsc_connect(card->reader);
     
+    apdu_t apdu;
+
+    u8 idbuf[2];
+    unsigned tag = 0xb800; // Control reference template for confidentiality (CT)
+    u8 buf[256];
+    size_t buf_len=256;
+
+    format_apdu(card, &apdu, APDU_CASE_4, 0x47, 0x81, 0x00);
+    apdu.lc = 2;
+    apdu.data = ushort2bebytes(idbuf, tag);
+    apdu.datalen = 2;         
+    apdu.le = ((buf_len >= 256) && !(card->caps & CARD_CAP_APDU_EXT)) ? 256 : buf_len;
+    apdu.resp = buf;          
+    apdu.resplen = buf_len;   
+
+
+    r = transmit_apdu(card, &apdu);
+    if(r<0) {
+        pcsc_disconnect(card->reader);
+    }
+    LOG_TEST_RET(r, "APDU transmit failed");
+
+    r = check_sw(card, apdu.sw1, apdu.sw2);
+    if(r<0) {
+        pcsc_disconnect(card->reader);
+    }
+    LOG_TEST_RET(r, "Card returned error");
+
     pcsc_disconnect(card->reader);
+
+    public_key = (unsigned char*)malloc(sizeof(unsigned char)*buf_len);
+    memcpy(public_key, buf, buf_len);
+    
     return 0;
 }
 
@@ -49,18 +86,51 @@ int csVerifyPIN(card_t *card, unsigned char* pin)
     int r;
 
     connect_card(card->reader, &card);
+    pcsc_connect(card->reader);
 
     pcsc_disconnect(card->reader);
     return 0;
 }
 
-int csDecipher(card_t *card, unsigned char* input, size_t in_length, 
+int csDecipher(card_t *card, unsigned char* input, size_t in_len,
                 unsigned char* output, size_t out_len)
 {
     int r;
 
     connect_card(card->reader, &card);
+    pcsc_connect(card->reader);
+
+    u8 *temp = NULL;
+    apdu_t   apdu;
+    /* There's some funny padding indicator that must be
+     * prepended... hmm. */
+    if (!(temp = (u8*)malloc(in_len + 1)))
+        return SC_ERROR_OUT_OF_MEMORY;
+    temp[0] = '\0';
+    memcpy(temp + 1, input, in_len);   
+    input = temp;
+    in_len += 1;
+
+    // Craft apdu
+    format_apdu(card, &apdu, APDU_CASE_4, 0x2A, 0x80, 0x86);
+    apdu.lc = in_len;
+    apdu.data = input;
+    apdu.datalen = in_len;
+    apdu.le = ((out_len >= 256) && !(card->caps & CARD_CAP_APDU_EXT)) ? 256 : out_len;
+    apdu.resp = output;
+    apdu.resplen = out_len;
+
+    r = transmit_apdu(card, &apdu); 
+    free(temp);
+    if(r<0)
+        pcsc_disconnect(card->reader);
+    LOG_TEST_RET(r, "APDU transmit failed\n");
+
+    r = check_sw(card, apdu.sw1, apdu.sw2);
+    if(r<0)
+        pcsc_disconnect(card->reader);
 
     pcsc_disconnect(card->reader);
-    return 0;
+
+    LOG_TEST_RET(r, "Card returned error\n");
 }
