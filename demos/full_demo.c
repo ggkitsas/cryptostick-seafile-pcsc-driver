dd81c306c715301214ea42ba85a5d08c8702f958
ggkitsas <ggkitsas@yahoo.com>
2014-07-21 17:36:34 +0300

    encrypting and decrypting using driver's API

--- demos/full_demo.c
+++ demos/full_demo.c
@@ -4,20 +4,22 @@
 
 int main()
 {
-    int i,j;
+    int i,j,r;
 
     cs_list csList;
     csListDevices(csList);
 
-//    pcsc_connect(csList.root->card->reader);
-    unsigned char pubkey[256];
-    csGetPublicKey(csList.root->card, pubkey);
-    printf("Public key: %s\n\n\n",pubkey);
+    card_t *card = csList.root->card;
 
+    // Get public key
+/*    unsigned char pubkey[256];
+    csGetPublicKey(card, pubkey);
+    printf("Public key: %s\n\n\n",pubkey);
+*/
     // Get serial number
     unsigned char serialNo[6];
     for(i=0; i<csList.numOfNodes; i++) {
-        csGetSerialNo(csList.root->card, serialNo);
+        csGetSerialNo(card, serialNo);
         printf("serialno: %s\n",serialNo);   
             for(j=2;j<6;j++) {
                 printf("%.2x ",csList.root->card->serialnr.value[j]);
@@ -25,13 +27,31 @@ int main()
         printf("\nReader name: %s\n", csList.root->card->reader->name);
     }
 
+    // Host encryption
+    unsigned char* encrypted;
+    unsigned encryptedLength;
+    unsigned char* input = (unsigned char*)"blablo";
+    r = csEncrypt(card,input , 6, &encrypted, &encryptedLength);
+    printf("Encrypted outside: ");
+    for(i=0;i<encryptedLength;i++)
+        printf("%.2x ",encrypted[i]);
+    printf("\n\n\n\n\n\n");
+
+    if(r!=0){
+        printf("Encrypt error\n");
+        return -1;
+    }
+    printf("EncLength= %d\n",encryptedLength);
+
+    // PIN
     unsigned char* pin = (unsigned char*)"123456";
-    csVerifyPIN(csList.root->card, pin);
+    csVerifyPIN(card, pin, 6);
 
+    // Card decryption
     unsigned char plain[6];
-    unsigned char* ciph = (unsigned char*)"abcabc";
-    csDecipher(csList.root->card, ciph, 6, plain, 6);
+    csDecipher(card, encrypted, encryptedLength, plain, 6);
 
-    // pcsc_disconnect(csList.root->card->reader);
+    printf("%s\n", plain);
+
     return 0;
 }
