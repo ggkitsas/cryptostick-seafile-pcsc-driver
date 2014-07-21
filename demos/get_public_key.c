7d601347cb69f7a4c4989e78ed7d8ef2ff473ddb
ggkitsas <ggkitsas@yahoo.com>
2014-07-21 03:32:03 +0300

    gnuk needs APDU_CASE_4_SHORT, opengpg APDU_CASE_4. Removed debug msgs

--- demos/get_public_key.c
+++ demos/get_public_key.c
@@ -8,18 +8,14 @@
 #include "pcsc-wrapper.h"
 #include "winscard.h"
 
+
+#define GNUK
+
 int main()
 {
     int r,i;
 
     apdu_t apdu;
-/*
-    card_t card;
-    card.cla = 0x00;
-    connect_card();
-    card_init(&card);
-*/
-
     // List readers
     reader_list* readerList = (reader_list*)malloc(sizeof(reader_list));
     r = pcsc_detect_readers(readerList);
@@ -28,13 +24,8 @@ int main()
         return -1;
     }
 
-//    card.reader = readerList->root->reader;
-
-
     card_t* card;
-    printf("%s %d\n",__FILE__, __LINE__);
     connect_card(readerList->root->reader, &card);
-    printf("%s %d\n",__FILE__, __LINE__);
     card_init(card);
 
     printf("Serial No: (len=%lu) ", card->serialnr.len - 2);
@@ -42,49 +33,37 @@ int main()
         printf("%.2x ", card->serialnr.value[i]);
     printf("\n");
 
-
-//    pcsc_connect(card.reader);
-    pcsc_connect(card->reader);
-
     u8 idbuf[2];
     unsigned tag = 0xb800; // Control reference template for confidentiality (CT)
-    u8 buf[256];
-    size_t buf_len=256;
+    u8 buf[270];
+    size_t buf_len=270;
 
     // Craft apdu
-//    format_apdu(&card, &apdu, APDU_CASE_4, 0x47, 0x81, 0x00);
+// GNUK specific
+#ifdef GNUK
+    format_apdu(card, &apdu, APDU_CASE_4_SHORT, 0x47, 0x81, 0x00);
+#else
     format_apdu(card, &apdu, APDU_CASE_4, 0x47, 0x81, 0x00);
+#endif
+    //format_apdu(card, &apdu, APDU_CASE_4, 0x47, 0x81, 0x00);
     apdu.lc = 2;
     apdu.data = ushort2bebytes(idbuf, tag);
     apdu.datalen = 2;         
-//    apdu.le = ((buf_len >= 256) && !(card.caps & CARD_CAP_APDU_EXT)) ? 256 : buf_len;
     apdu.le = ((buf_len >= 256) && !(card->caps & CARD_CAP_APDU_EXT)) ? 256 : buf_len;
     apdu.resp = buf;          
     apdu.resplen = buf_len;   
 
-    printf("%s %d\n",__FILE__, __LINE__);
-//    r = transmit_apdu(&card, &apdu);
+printf("------------------------------------------------------------------------- START ---------------------------------------------------\n");
     r = transmit_apdu(card, &apdu);
-    if(r<0) {
-//        pcsc_disconnect(card.reader);
-        pcsc_disconnect(card->reader);
-    }
     LOG_TEST_RET(r, "APDU transmit failed");                                                                                                                                                                                
 
-//    r = check_sw(&card, apdu.sw1, apdu.sw2);
     r = check_sw(card, apdu.sw1, apdu.sw2);
-    if(r<0) {
-//        pcsc_disconnect(card.reader);
-        pcsc_disconnect(card->reader);
-    }
     LOG_TEST_RET(r, "Card returned error");                                                                                                                                                                                 
 
 
     for(i=0;i<buf_len;i++)
         printf("%.2x ",buf[i]);
 
-//    pcsc_disconnect(card.reader);
-    pcsc_disconnect(card->reader);
     LOG_FUNC_RETURN(apdu.resplen); 
     return 0;
 }
