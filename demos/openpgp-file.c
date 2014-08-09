3b9bead815bbba7358599d92f6591bc06efd2484
ggkitsas <ggkitsas@yahoo.com>
2014-08-09 19:03:43 +0300

    parsing openpgp file

--- demos/openpgp-file.c
+++ demos/openpgp-file.c
@@ -4,9 +4,15 @@
 
 #include "openpgp-msg.h"
 
+
+
 int main()
 {
-    
+    pgp_message msg;
+    pgp_read_msg_file("/home/cyc0/Desktop/sub.pubring.gpg", &msg);
+ 
+    printf("Type: %d\n", msg.packet_type);
+    pgp_print_pubkey_packet( (pgp_pubkey_packet*)(msg.pgp_packet) );
     
     return 0;
 }
